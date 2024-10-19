c6978d799a4a84bff7869f81da59e8af992db7f4
Karol Bąk <kukicola@gmail.com>
2024-10-19 17:56:48 +0200

    Initial Commit

---
+++ Web Analytics/Web_AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_AnalyticsApp.swift
+//  Web Analytics
+//
+//  Created by Karol Bąk on 19/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Web_AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
